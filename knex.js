8cb7b0ad4e07020da15bc7dd9b646133a0c680c9
robotsintheattic <katherine.r.johnson@gmail.com>
2017-05-30 17:02:55 -0600

    books patch route functionality

--- knex.js
+++ knex.js
@@ -4,4 +4,21 @@ const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('./knexfile')[environment];
 const knex = require('knex')(knexConfig);
 
+// exports.up = (knex) => {
+//   return knex.schema.createTable('books', (table) => {
+//     table.increments();
+//     table.string('title', 255).notNullable().defaultTo('');
+//     table.string('author', 255).notNullable().defaultTo('');
+//     table.string('genre', 255).notNullable().defaultTo('');
+//     table.string('description').notNullable().defaultTo('');
+//     table.string('cover_url').notNullable().defaultTo('');
+//     table.timestamp('created_at').defaultTo(knex.fn.now());
+//     table.timestamp('updated_at').defaultTo(knex.fn.now());
+//   })
+// };
+//
+// exports.down = (knex) => {
+//   return knex.schema.dropTable('books');
+// };
+
 module.exports = knex;
--- routes/books.js
+++ routes/books.js
@@ -6,5 +6,78 @@ const express = require('express');
 const router = express.Router();
 
 // YOUR CODE HERE
+var knex = require('../knex');
+var humps = require('humps');
+
+
+//GET all books
+router.get('/books', (req, res, next) => {
+  knex('books')
+    .orderBy('title')
+    .then((books) => {
+      res.send(humps.camelizeKeys(books));
+    });
+});
+
+//GET one book
+router.get('/books/:id', (req, res, next) => {
+  knex('books')
+    .orderBy('title')
+    .where('id', req.params.id)
+    .then((books) => {
+      res.send(humps.camelizeKeys(books[0]));
+    });
+});
+
+//POST one book
+router.post('/books', (req, res, next) => {
+  let body = req.body;
+  // title,author,genre,description,coverUrl
+  // INSERT INTO books (title,author,genre,description,coverUrl) VALUES();
+  //req.body
+  knex('books')
+    .returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+    .insert({
+      title: body.title,
+      author: body.author,
+      genre: body.genre,
+      description: body.description,
+      cover_url: body.coverUrl
+    })
+    .then((books) => {
+      res.send(humps.camelizeKeys(books[0]));
+    })
+    // .done()
+});
+
+//UPDATE one book
+router.patch('/books/:id', (req, res, next) => {
+  let body = req.body;
+  let id = req.params.id;
+  knex('books')
+    .returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+    .where('id', id)
+    .update({
+      title: body.title,
+      author: body.author,
+      genre: body.genre,
+      description: body.description,
+      cover_url: body.coverUrl
+    })
+    .then((books) => {
+      res.send(humps.camelizeKeys(books[0]));
+    })
+});
+
+//DELETE one book
+router.delete('/books/:id', (req, res, next) => {
+  let id = req.params.id;
+  knex('books')
+    .where('id', id)
+    .del()
+    .then((books) => {
+      res.sendStatus(humps.camelizeKeys(books));
+    })
+})
 
 module.exports = router;
